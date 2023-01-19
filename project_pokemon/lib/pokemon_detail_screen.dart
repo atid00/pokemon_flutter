e0dd00419de92a8ed5cc1bdf899ff07cfd6500af
atid utagang <atid.u@kkumail.com>
2023-01-19 22:30:30 +0700

    Initial commit

---
+++ project_pokemon/lib/pokemon_detail_screen.dart
@@ -0,0 +1,52 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:project_pokemon/pokemon_model.dart';
+import 'package:project_pokemon/pokemon_service.dart';
+import 'pokemon_list_model.dart';
+
+class PokemonDetailScreen extends StatelessWidget {
+  PokemonDetailScreen({required this.title, required this.url});
+  String title;
+  String url;
+  int number = 11;
+  PokemonService pokemonService = PokemonService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(this.title),
+        ),
+        body: FutureBuilder(
+          future: pokemonService.getdata(url),
+          builder: (context, AsyncSnapshot<PokemonModel> snapshot) {
+            if (snapshot.hasData) {
+              PokemonModel data = snapshot.data!;
+              return Scaffold(
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.purple.shade200,
+                        backgroundImage: NetworkImage(
+                          data.sprites!.frontDefault.toString(),
+                        ),
+                        radius: 99,
+                      ),
+                      Text("Name: " + data.name!),
+                      Text("ID: " + data.id.toString()),
+                      Text("Weight: " + data.weight.toString()),
+                      Text("Height: " + data.height.toString()),
+                    ],
+                  ),
+                ),
+              );
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
+        ));
+  }
+}
